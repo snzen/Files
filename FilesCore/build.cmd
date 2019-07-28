5e68ead875d76e5ed0908d0ea7da6666298788f2
arsuq <arsuq@ryzen.home>
2019-07-28 13:54:04 +0300

    ..

--- FilesCore/build.cmd
+++ FilesCore/build.cmd
@@ -1,4 +1,5 @@
-﻿dotnet publish -c release -r win-x64 --self-contained false
+﻿dotnet restore
+dotnet publish -c release -r win-x64 --self-contained false
 dotnet publish -c release -r win-arm64 --self-contained false
 dotnet publish -c release -r linux-x64 --self-contained false
 dotnet publish -c release -r linux-arm --self-contained false
